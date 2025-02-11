44c11222d0be1b3fff046bdf118d449d670b35c6
alkeryn <plbraundev@gmail.com>
2025-02-11 14:55:54 +0000

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use tech_test::penumbra::*;
+use clap::Parser;
+#[derive(Parser)]
+struct Args {
+    #[arg(short, default_value = "https://grpc.penumbra.silentvalidator.com")]
+    node_addres: String
+}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let node = "https://grpc.penumbra.silentvalidator.com";
-    let pen = Penumbra::new(node).await?;
+    let args = Args::parse();
+    let node = args.node_addres;
+    let pen = Penumbra::new(&node).await?;
     let block = pen.get_penumbra_lattest_block_height().await?.unwrap();
     println!("{}", block);
     let b = pen.get_block_n(block as i64).await?;
